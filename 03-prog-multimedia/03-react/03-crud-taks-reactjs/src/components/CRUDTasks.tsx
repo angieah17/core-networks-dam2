d4be788bca63a0aa2957582792a790020fc81dfb
Angie Amado <angieamado76@gmail.com>
2026-01-18 13:22:58 +0100

    feat: create useStates

--- 03-prog-multimedia/03-react/03-crud-taks-reactjs/src/components/CRUDTasks.tsx
+++ 03-prog-multimedia/03-react/03-crud-taks-reactjs/src/components/CRUDTasks.tsx
@@ -1,6 +1,54 @@
+import { useState } from "react";
+
+type Task = {
+    id: number,
+    title: string,
+    assignedTo: string,
+    priority: string,
+    completed: boolean,
+}
+
+type TaskForm = {
+    title: string,
+    assignedTo: string,
+    priority: string,
+    completed: boolean,
+}
+
 
 export default function CRUDTasks() {
+
+    //I.Definicion de los estados
+
+    //1. task
+    const [task, setTask] = useState<Task | null>(null);
+
+    //2. tasks
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    //3. error (permite personalizar los errores)
+    const [error, setError] = useState<string | null>(null);
+
+    //4. loading 
+    const [loading, setLoading] = useState<boolean>(false);
+
+    //5. editingId (si es null CREA y si es number ACTUALIZA)
+    const [editingId, setEditingId] = useState<number | null>(null);
+    //6. formData
+
+    const formDataEnBlanco : TaskForm = {
+        title: '',
+        assignedTo: '',
+        priority: '',
+        completed: false,
+    }
+
+    const [formData, setFormData] = useState<TaskForm>(formDataEnBlanco);
+
+
   return (
-    <>CRUDTasks</>
+    <>
+    
+    </>
   )
 }
