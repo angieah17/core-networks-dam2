3b081bf0f6160aff79fedb518f8a156c73828a52
Angie <angieamado76@gmail.com>
2026-02-02 13:54:15 +0100

    feat: agrega métodos ManejadorCliente

--- 05-prog-servicios-procesos/practica-sockets/src/socket/examen/ServidorSocket.java
+++ 05-prog-servicios-procesos/practica-sockets/src/socket/examen/ServidorSocket.java
@@ -10,19 +10,19 @@ public class ServidorSocket {
 	private static final int PUERTO = 8081;
 	
 
-
+	//2. Main para dejar la clase en un bucle infinito
 	public static void main(String[] args) {
-		
-		
-		try (ServerSocket serverSocket = new ServerSocket(PUERTO)) {
+				
+		try (ServerSocket serverSocket = new ServerSocket(PUERTO)) { //El serverSocket se crea a partir de un puerto, comienza a eschar
 			
 			System.out.println("====SERVIDOR SOCKET INICIADO ====");
 			
 			//Bucle infinito para aceptar múltiples clientes
 			
 			while (true) {
-				Socket clienteSocket = serverSocket.accept();
+				Socket clienteSocket = serverSocket.accept(); //Bloquea el hilo hasta que llega un cliente y cuando se genera devuelve un objeto socket
 				
+				//Por cada clienteSocket (por cada conexión establecida se crea un hilo)
 				Thread hiloCliente = new Thread(new ManejadorCliente(clienteSocket));
 				hiloCliente.start();
 				
